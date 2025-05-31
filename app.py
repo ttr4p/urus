b566a73d28820f2973b32f4b29102f31128f3fac
Elson Neto <109770308+ttr4p@users.noreply.github.com>
2025-05-31 20:30:29 -0300

    fix: corrigido host e porta para rodar no Railway

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import pymysql
 import os
 
 app = Flask(__name__)
-app.secret_key = 'secreto123'  # Use algo seguro em produção
+app.secret_key = 'secreto123'
 
-# Configurações do banco via variáveis de ambiente do Railway
+# Configurações do banco (Railway)
 db_config = {
     'host': os.getenv("MYSQLHOST"),
     'user': os.getenv("MYSQLUSER"),
@@ -56,4 +56,4 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
